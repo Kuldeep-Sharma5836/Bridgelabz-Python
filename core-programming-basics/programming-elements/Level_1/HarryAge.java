f9e9eaf8cc7beb2aa475f356d1f092b1048cb24b
kuldeepsharma5836 <panditkuldeep2019@gmail.com>
2026-04-13 19:20:48 +0530

    core programming

--- core-programming-basics/programming-elements/Level_1/HarryAge.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class HarryAge {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int birthYear = sc.nextInt();
-        int currentAge = 2024-birthYear;
-        System.out.println("Harry age in 2024 is: " + currentAge);
-    }
-}
