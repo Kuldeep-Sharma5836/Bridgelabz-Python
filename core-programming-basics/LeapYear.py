6d8553051eb23108967529b8efb0dd59ec4b0aa8
kuldeepsharma5836 <panditkuldeep2019@gmail.com>
2026-04-14 09:02:36 +0530

    [Kuldeep Sharma] Leap Year

---
+++ core-programming-basics/LeapYear.py
@@ -0,0 +1,6 @@
+year=int(input("Enter the year "))
+if(len(str(year))>=4):
+    if(year%4==0)or(year%100==0 and year%400==0):
+        print(year,"is a leap year")
+    else:
+        print(year,"is not a leap year")
