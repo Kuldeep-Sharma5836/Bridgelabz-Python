38042af9fb98d4a98171c3d01d7640109fa578c0
kuldeepsharma5836 <panditkuldeep2019@gmail.com>
2026-04-15 16:47:20 +0530

    [Kuldeep Sharma] Solve Gambler problem

--- Logical-Program/Gambler.py
+++ Logical-Program/Gambler.py
@@ -3,42 +3,24 @@ import random
 stake = int(input("Enter stake: "))
 goal = int(input("Enter goal: "))
 trials = int(input("Enter number of trials: "))
-bet = int(input("Enter bet amount: "))
 
 wins = 0
-total_win = 0
-total_loss = 0
+bets = 0
 
 for i in range(trials):
     cash = stake
-    print("\n--- Trial", i + 1, "started ---")
 
-    if cash <= 0:
-        print("No money to play.")
-    else:
-        while cash > 0:
-            if random.random() < 0.5:
-                cash += bet
-                total_win += bet
-                print("Won:", bet, ", Current Cash:", cash)
-            else:
-                cash -= bet
-                total_loss += bet
-                print("Lost:", bet, ", Current Cash:", cash)
+    while cash > 0 and cash < goal:
+        bets += 1
+        if random.random() < 0.5:
+            cash += 1   
+        else:
+            cash -= 1   
 
-            if cash >= goal:
-                break
-
-    print("Trial", i + 1, "ended with cash:", cash)
-
-    if cash >= goal:
-        print("Result: win")
+    if cash == goal:
         wins += 1
-    else:
-        print("Result: loss ")
 
-print("\nFinal Results ----------------------")
-print("Total Wins:", wins)
-print("Total Loss Trials:", trials - wins)
-print("Total Winning Amount:", total_win)
-print("Total Losing Amount:", total_loss)
+print("Wins:", wins)
+print("Total Bets:", bets)
+print("Win percent =", (wins / trials) * 100)
+print("Loss percent =", ((trials - wins) / trials) * 100)
